8fe9068f06c9533f5c45e30196663efd88c4f7e3
DavidBarbosag <david.barbosa-g@mail.escuelaing.edu.com>
2025-04-28 21:22:51 -0500

    modelos de las clases RecreativerElements y Register

--- Register/models.py
+++ Register/models.py
@@ -1,8 +1,37 @@
 from django.db import models
+from Reservation.models import Reservation
 
 class Register(models.Model):
+    """
+       Represents a register .
 
-    id = models.CharField(primary_key=True, max_length=15, blank=False)
+       Attributes:
+            registerID (int): Unique identifier for the Register (used as the primary key).
+            reservationID (str): Unique identifier of the Reservation.
+            remainingElements (JSONField): JSONField of the remaining elements represented as follows
+            {'codigo': str, 'nombre': str, 'estado': str, 'cantidad': int}
+            returnedElements (JSONField): JSONField of the returned elementsrepresented as follows
+            {'codigo': str, 'nombre': str, 'estado': str, 'cantidad': int}
+
+       """
+
+    class StatusChoices(models.TextChoices):
+        RETURNED_GOOD = 'RETURNED_GOOD', 'Returned in Good Condition'
+        RETURNED_DAMAGED = 'RETURNED_DAMAGED', 'Returned Damaged'
+        NOT_RETURNED = 'NOT_RETURNED', 'Not Returned'
+
+    registerId = models.AutoField(primary_key=True)
+    reservationId = models.ForeignKey(Reservation, on_delete=models.PROTECT, related_name='registers')
+    returnedElements = models.JSONField(blank=False, default=dict)
+    remainingElements = models.JSONField(blank=True, default=dict)
+
+    class Meta:
+        verbose_name = 'Register'
+        verbose_name_plural = 'Registers'
+        ordering = ['reservationId']
+        indexes = [
+            models.Index(fields=['reservation']),
+        ]
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.registerId}"
